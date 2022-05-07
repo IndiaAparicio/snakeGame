a21bf9c58f24fb84d93d9a6af41e55fe7fb89b30
India Aparicio <aparicioindia@gmail.com>
2022-05-07 11:57:38 +0200

    added classes new files

---
+++ mySnakeGame/src/ofFood.cpp
@@ -0,0 +1,8 @@
+//
+//  ofFood.cpp
+//  mySnakeGame
+//
+//  Created by India Aparicio on 07.05.22.
+//
+
+#include "ofFood.hpp"
---
+++ mySnakeGame/src/ofFood.h
@@ -0,0 +1,13 @@
+//
+//  ofFood.hpp
+//  mySnakeGame
+//
+//  Created by India Aparicio on 07.05.22.
+//
+
+#ifndef ofFood_hpp
+#define ofFood_hpp
+
+#include <stdio.h>
+
+#endif /* ofFood_hpp */
